daeaaae9d2a8a615bfc675f8bb6f7534b394350d
Alina <71149650+deviati0n@users.noreply.github.com>
2022-01-19 09:42:31 +0300

    adding a dataclass for a report

--- pars_session_sel.py
+++ pars_session_sel.py
@@ -13,17 +13,39 @@ driver.get(link)
 all_session = find_elements(By.XPATH, '//*[@id="INTERNET_ONLINE_"]//*/td[1]/a')
 
 
-# for loop in func_bill 
-
+# -------------
 from datetime import datetime, timedelta, date
 
-today = date.today()
+date_bill = {'first_date':date_obj, 'last_date':today}
 
-date_str = '2022-01-17'
-date_obj = datetime.strptime(date_str, '%Y-%m-%d').date()
+def report_billing(date):
+    # ...
 
-while date_obj <= today:
-    print(date_obj)
-    date_obj = date_obj + timedelta(days = 1)
+    while date[first_date] <= date[last_date]:
+
+        #func: func_name(date[first_date])
+
+       date[first_date] = date[first_date] + timedelta(days = 1)
 
 
+# -------------
+# dataclass for report
+from dataclasses import dataclass
+from typing import Dict
+
+@dataclass
+class BillingReport:
+    billing_data: Dict[str, int]
+        
+    def report(self) -> str:
+        repReq = f'Заявки - {self.billing_data["dayReq"]}/{self.billing_data["periodReq"]}'
+        repCon = f'Подключения - {self.billing_data["dayCon"]}/{self.billing_data["periodCon"]}'
+        repSumm = f'Сумма - {self.billing_data["daySumm"]}/{self.billing_data["periodSum"]}/{self.billing_data["percent"]}%'
+        
+        return f'{repReq}\n{repCon}\n{repSumm}'
+         
+         
+bill = {'dayReq':1, 'periodReq':12, 'dayCon': 1, 'periodCon': 12, 'daySumm': 2, 'periodSum': 12, 'percent': 52.3}
+result = BillingReport(bill)
+
+print(result.report())
